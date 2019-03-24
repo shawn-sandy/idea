1ca88046235b60f1c106ceac61b3913a87fe99c8
shawnsandy <shawnsandy04@gmail.com>
2019-03-24 10:33:58 -0400

    Adds compatibility warning for ie users

--- dist/js/ie.js
+++ dist/js/ie.js
@@ -3,7 +3,7 @@
 /**
  * Detect if any version of Internet is running
  */
-const isIE = function () {
+var isIE = function () {
   var ua = window.navigator.userAgent; //Check the userAgent property of the window.navigator object
   var msie = ua.indexOf('MSIE '); // IE 10 or older
   var trident = ua.indexOf('Trident/'); //IE 11
@@ -14,4 +14,5 @@ const isIE = function () {
 if(isIE()) {
   const ieElm = document.getElementById('ie-warning')
   ieElm.classList.remove('ie-hidden')
+  console.info("User is using IE");
 }
