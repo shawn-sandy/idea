74defca2fec9c2aa7ac833f92e0a0f4faa9b49aa
shawnsandy <shawnsandy04@gmail.com>
2019-05-27 14:16:34 -0400

    updates the npm filter results / output

--- src/_shortcodes/npm.js
+++ src/_shortcodes/npm.js
@@ -1,4 +1,3 @@
-const Got = require("got")
 const axios = require('axios')
 
 module.exports =  async( packageName, callback ) => {
@@ -6,10 +5,25 @@ module.exports =  async( packageName, callback ) => {
 		const url = `https://api.npms.io/v2/package/${ packageName }`;
     const response = await axios.get( url );
     // const response = await Got( url );
+    const data = response ? response.data.collected.metadata : null
+    const github = response ? response.data.collected.github : null
+    const npm = response ? response.data.collected.npm : null
+    const source = response ? response.data.collected.source : null
+    const stars = github ? github.starsCount : 'n/a'
+    const openIssues = github ? github.issues.openCount : 'n/a'
     console.log('url', url)
-    console.log(response.data.collected.metadata.description)
+    console.log(data)
+    console.log('github', github)
 		// const downloads = JSON.parse( body ).downloads;
-		const downloads = `${response.data.collected.metadata.description}`
+    const downloads = `
+    <ul class="package list">
+    <li class="text-capitalize">${data.name || null}</li>
+    <li>Description ${data.description || null}</li>
+    <li>Version ${data.version || null}</li>
+    <li>Stars Count ${stars}</li>
+    <li>Open issues ${openIssues}</li>
+    </ul>
+    `
     callback( null, downloads);
 	}
 	catch( error ){
