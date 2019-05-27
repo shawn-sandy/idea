3b573bfab5211158a3581b0603139234c7ddc677
shawnsandy <shawnsandy04@gmail.com>
2019-05-27 11:46:15 -0400

    Changes api url - api.npms.io

--- src/_shortcodes/npm.js
+++ src/_shortcodes/npm.js
@@ -2,16 +2,12 @@ const Got = require("got")
 
 module.exports =  async( packageName, callback ) => {
 	try {
-		const url = `https://api.npmjs.org/downloads/point/last-week/${ packageName }`;
+		const url = `https://api.npms.io/v2/package/${ packageName }`;
     const { body } = await Got( url );
     console.log(body)
     console.log('url', url)
 		// const downloads = JSON.parse( body ).downloads;
-		const downloads = `
-    <div>${ JSON.parse( body ).package }</div>
-    <div>${ JSON.parse( body ).downloads }</div>
-    <div>${ JSON.parse( body ).start }</div>
-    `
+		const downloads = `data here`
     callback( null, downloads);
 	}
 	catch( error ){
