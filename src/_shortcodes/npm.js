b02a169f3a752ace8b071ff0e2d19ae3c8a6c038
shawnsandy <shawnsandy04@gmail.com>
2019-05-27 12:09:29 -0400

    Replaces got service with axios

--- src/_shortcodes/npm.js
+++ src/_shortcodes/npm.js
@@ -1,13 +1,15 @@
 const Got = require("got")
+const axios = require('axios')
 
 module.exports =  async( packageName, callback ) => {
 	try {
 		const url = `https://api.npms.io/v2/package/${ packageName }`;
-    const { body } = await Got( url );
-    console.log(body)
+    const response = await axios.get( url );
+    // const response = await Got( url );
     console.log('url', url)
+    console.log(response.data.collected.metadata.description)
 		// const downloads = JSON.parse( body ).downloads;
-		const downloads = `data here`
+		const downloads = `${response.data.collected.metadata.description}`
     callback( null, downloads);
 	}
 	catch( error ){
