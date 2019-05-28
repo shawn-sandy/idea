252aec266bd606e285bd05292166606c99200f8f
shawnsandy <shawnsandy04@gmail.com>
2019-05-27 21:35:47 -0400

    Cleans up the npm filter

--- src/_shortcodes/npm.js
+++ src/_shortcodes/npm.js
@@ -4,9 +4,8 @@ module.exports =  async( packageName, callback ) => {
 	try {
 		const url = `https://api.npms.io/v2/package/${ packageName }`;
     const response = await axios.get( url );
-    // const response = await Got( url );
     const data = response ? response.data.collected.metadata : null
-    const github = response ? response.data.collected.github : null
+    const github = response.data.collected.github ? response.data.collected.github : null
     const npm = response ? response.data.collected.npm : null
     const source = response ? response.data.collected.source : null
     const stars = github ? github.starsCount : 'n/a'
@@ -16,12 +15,12 @@ module.exports =  async( packageName, callback ) => {
     console.log('github', github)
 		// const downloads = JSON.parse( body ).downloads;
     const downloads = `
-    <ul class="package list">
-    <li class="text-capitalize">${data.name || null}</li>
-    <li>Description ${data.description || null}</li>
-    <li>Version ${data.version || null}</li>
-    <li>Stars Count ${stars}</li>
-    <li>Open issues ${openIssues}</li>
+    <ul class="npm-package-list">
+    <li class="text-capitalize npm-pakage-name">${data.name}</li>
+    <li class="npm-pakage-description">Description ${data.description}</li>
+    <li class="npm-pakage-version">Version ${data.version}</li>
+    <li class="npm-pakage-stars">Stars Count ${stars}</li>
+    <li class="npm-package-issues">Open issues ${openIssues}</li>
     </ul>
     `
     callback( null, downloads);
