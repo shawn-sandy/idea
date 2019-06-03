d757acf0eb692571c66a063f771a71a86ba81484
shawnsandy <shawnsandy04@gmail.com>
2019-06-03 09:30:10 -0400

    Updates the atomic  collections methods - refactors the sorting methods - adds atomic

--- packages/eleventy_atomic_plugin/.eleventy.js
+++ packages/eleventy_atomic_plugin/.eleventy.js
@@ -1,48 +1,34 @@
 // imports
 
-
+const sorting = (collection, tags = "atoms") => {
+  return collection.getFilteredByTag(tags).sort((a, b) => {
+    if (a.data.title > b.data.title) return -1
+    else if (a.data.title < b.data.title) return 1
+    else return 0
+  })
+}
 module.exports = (eleventyConfig) => {
 
   //
 
   eleventyConfig.addCollection("atoms", function(collection) {
-    return collection.getFilteredByTag("atoms").sort((a, b) => {
-      if (a.data.title > b.data.title) return -1
-      else if (a.data.title < b.data.title) return 1
-      else return 0
-    })
+    return sorting(collection, "atoms")
   });
 
   eleventyConfig.addCollection("molecules", function(collection) {
-    return collection.getFilteredByTag("molecules").sort((a, b) => {
-      if (a.data.title > b.data.title) return -1
-      else if (a.data.title < b.data.title) return 1
-      else return 0
-    })
+    return sorting(collection, "molecules")
   });
 
   eleventyConfig.addCollection("organisms", function(collection) {
-    return collection.getFilteredByTag("organisms").sort((a, b) => {
-      if (a.data.title > b.data.title) return -1
-      else if (a.data.title < b.data.title) return 1
-      else return 0
-    })
+    return sorting(collection, "organisms")
   });
 
   eleventyConfig.addCollection("pages", function(collection) {
-    return collection.getFilteredByTag("pages").sort((a, b) => {
-      if (a.data.title > b.data.title) return -1
-      else if (a.data.title < b.data.title) return 1
-      else return 0
-    })
+    return sorting(collection, "pages")
   });
 
   eleventyConfig.addCollection("templates", function(collection) {
-    return collection.getFilteredByTag("templates").sort((a, b) => {
-      if (a.data.title > b.data.title) return -1
-      else if (a.data.title < b.data.title) return 1
-      else return 0
-    })
+    return sorting(collection, "templates")
   });
 
 }
--- packages/eleventy_atomic_plugin/README.md
+++ packages/eleventy_atomic_plugin/README.md
@@ -1,11 +1,33 @@
-# `@shawnsandy/eleventy_atomic_plugin`
+# Atomic
 
-> TODO: description
+A plugin for building and managing an atomic driven design system with Eleventy
+
+> Atomic design is a methodology composed of five distinct stages working together to create interface design systems in a more deliberate and hierarchical manner.
+> The five stages
 
 ## Usage
 
-```
+Import the plugin into to your `.eleventy.js` file
+
+``` js
+
 const eleventyAtomicPlugin = require('@shawnsandy/eleventy_atomic_plugin');
 
-// TODO: DEMONSTRATE API
+```
+
+Add the plugin to the `export.modules` section
+
+``` js
+eleventyConfig.addPlugin(eleventyAtomicPlugin);
+
+```
+
+Add the collection to your template(s) `eleventy.js file` <a href="https://www.11ty.io/docs/collections/" target="_blank">collections info</a>
+
+``` html
+{% for atom in collections.atoms %}
+{{ atom.data.Title }}
+    {{ atom.templateContent | safe }}
+{% endfor %}
+
 ```
