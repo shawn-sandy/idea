0c38f6051aaaef0e345e88a868f9df1dbdd8f140
shawnsandy <shawnsandy04@gmail.com>
2019-06-09 00:35:21 -0400

    Updates the elements shortcode

--- packages/ideacore/lib/elements.js
+++ packages/ideacore/lib/elements.js
@@ -23,15 +23,29 @@ module.exports = {
     return `<input value="${value}" class="${class_names}" id="${id}" name="${id}" type="${type}" placeholder="Enter ${content}" ${attrs}>`
   },
 
-  textarea: (content, class_names, attrs) => {
-    return `<textarea class="${class_names}" ${attrs} >${content || ''}</textarea>`
+  textarea: (content, id, class_names, attrs) => {
+    return `<textarea id="${id}" name="${id} class="${class_names}" ${attrs} >${content || ''}</textarea>`
   },
 
-  select: () =>{
-    return ``
+  select: (content, class_names, attrs) =>{
+    return `<select class="${class_names} ${attrs}></select>`
   },
 
-  selectOpt: () => {
-    return ``
+  selectOpt: (content, value) => {
+    return `<option value="${value}>${content}</option>`
+  },
+
+  list_ul: (content, class_names, attrs) => {
+    return `<ul class="${class_names} ${attrs}>${content}</ul>`
+  },
+
+  list_li: (content, class_names, attrs) => {
+    return `<li class="${class_names} ${attrs}>${content}</li>`
+  },
+
+  blockquote: (content, class_names, attrs) => {
+    return `<blockquote class="${class_names} ${attrs}>${content}</blockquote>`
   }
+  
+
 };
