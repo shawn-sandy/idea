8836a6212bf9d15a9a5433c3111e204a161c967b
shawnsandy <shawnsandy04@gmail.com>
2019-06-23 13:13:13 -0400

    Html5 from validation feature

--- packages/blacktie-js/src/validation.js
+++ packages/blacktie-js/src/validation.js
@@ -1,28 +1,65 @@
+/* eslint-disable no-unused-vars */
+var jsValidate = function() {
+
+  var methods = {};
+
+  var forEach = function(arr, callback) {
+      Array.prototype.forEach.call(arr, callback);
+    };
+
+  methods.validate = function(formID, inputElm, options = {}) {
+
+    var borderClass = options.borderClass || "border_danger";
+    var messageElm = options.messages || ".js-error-message"
 
 
-const jsValidate = () => {
-  const _form = document.getElementById(formID)
-  const inputs = _form.querySelectorAll(elements)
-  forEach(inputs, function (input) {
-    input.addEventListener('invalid', function (event) {
-      input.classList.add('border-danger')
-      /*  showing custom error message if we have in data attribute  */
-      const customMessage = getCustomMessage(input)
-      appendValidationMsg(input, customMessage)
-    }, false)
+    var _form = document.getElementById(formID);
+    var inputs = _form.querySelectorAll(inputElm);
 
-    input.addEventListener('blur', function (event) {
-      input.classList.remove('border-danger')
-      if (!input.checkValidity()) {
-        input.classList.add('border-danger')
-        /*  showing custom error message if we have in data attribute  */
-        var customMessage = getCustomMessage(input)
-        appendValidationMsg(input, customMessage)
-      } else {
-        appendValidationMsg(input, '*') // reset the message
-      }
-    })
-  })
-}
+   forEach(inputs, function(input) {
+      input.addEventListener(
+        "invalid",
+        function(e) {
+          input.classList.add(borderClass);
+          /*  showing custom error message if we have in data attribute  */
+          const customMessage = getCustomMessage(input);
+          appendValidationMsg(input, customMessage, messageElm);
+        },
+        false
+      );
 
-export default jsValidate
+      input.addEventListener("blur", function(e) {
+        input.classList.remove(borderClass);
+        if (!input.checkValidity()) {
+          input.classList.add(borderClass);
+          /*  showing custom error message if we have in data attribute  */
+          var customMessage = getCustomMessage(input);
+          appendValidationMsg(input, customMessage, messageElm);
+        } else {
+          appendValidationMsg(input, "*", messageElm); // reset the message
+        }
+      });
+    });
+
+  }
+
+  function appendValidationMsg(inputElement, message, messageElm) {
+    var parent = inputElement.parentNode;
+    var errMsg = parent.querySelector(messageElm);
+    // errMsg.textContent = '*'
+    errMsg && (errMsg.textContent = message || inputElement.validationMessage);
+  }
+
+  function getCustomMessage(inputElement) {
+    if (inputElement.validity.valueMissing) {
+      return inputElement.dataset.requirederror;
+    } else if (inputElement.validity.typeMismatch) {
+      return inputElement.dataset.typeerror;
+    } else {
+      return false;
+    }
+  }
+
+  return methods;
+
+};
