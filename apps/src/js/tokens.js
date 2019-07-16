3d6e320df20b5a2bfe0ff6c0244e67b219633533
shawnsandy <shawnsandy04@gmail.com>
2019-07-16 19:19:03 -0400

    Update color tokens - install rollup

--- apps/src/js/tokens.js
+++ apps/src/js/tokens.js
@@ -1,18 +1,21 @@
 /**
  * Do not edit directly
- * Generated on Thu, 11 Jul 2019 03:57:28 GMT
+ * Generated on Mon, 15 Jul 2019 01:59:23 GMT
  */
 
-export const BtnPadding = "10rem"; // default button padding
-export const BtnFontSize = 2;
-export const BtnPrimaryBackgroundColor = "hsl(10, 80, 50)";
-export const BtnPrimaryColor = "#000000";
-export const BtnNew = "#61dafb";
-export const ColorBasePrimary = "#61dafb";
-export const ColorBaseSecondary = "#fc7aa5";
-export const ColorText = "#666666";
-export const ColorBackground = "#eeeeee";
-export const Primary = "#00548c";
-export const Secondary = "#00B1E2";
-export const Modal = "lighten(#97999B, 15%)";
-export const CompareTray = "lighten(#21314D, 20%)";
+export const CompBtnPadding = "1.25rem 5rem";
+export const CompBtnFontSize = "1.07rem";
+export const CompBtnBackgroundColor = {"value":"#232323","comment":"Element background etc","original":{"value":"#232323","comment":"Element background etc"},"name":"ColorDark100","attributes":{"category":"color","type":"dark","item":"100"},"path":["color","dark","100"]};
+export const CompBtnHoverBackgroundColor = "color.blue";
+export const ColorBlue = "#3f86f5"; // Color used for logos / highlights
+export const ColorGray = "#bebdbf"; // a midrange gray backgrounds/muted text
+export const ColorLightGray = "#efefef"; // light background color
+export const ColorDark25 = "#707070"; // highlight text
+export const ColorDark50 = "#2a2a2a"; // default text
+export const ColorDark100 = "#232323"; // Element background etc
+export const ColorKey = "colors";
+export const ButtonKey = "btns";
+export const HeaderKey = "headers";
+export const FontsKeys = "fonts";
+export const FontTypesKeys = "font-type";
+export const MarginPaddingKeys = "spacers";
