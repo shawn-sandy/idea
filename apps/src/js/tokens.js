04f138212fd26a50bfe3c4e1c046d903c4b4bd6b
shawnsandy <shawnsandy04@gmail.com>
2019-07-23 22:30:34 -0400

    Updates - add components mixin - updates the color generator mexin - updates generator functions

--- apps/src/js/tokens.js
+++ apps/src/js/tokens.js
@@ -1,18 +1,18 @@
 /**
  * Do not edit directly
- * Generated on Mon, 15 Jul 2019 01:59:23 GMT
+ * Generated on Wed, 24 Jul 2019 01:42:36 GMT
  */
 
-export const CompBtnPadding = "1.25rem 5rem";
-export const CompBtnFontSize = "1.07rem";
-export const CompBtnBackgroundColor = {"value":"#232323","comment":"Element background etc","original":{"value":"#232323","comment":"Element background etc"},"name":"ColorDark100","attributes":{"category":"color","type":"dark","item":"100"},"path":["color","dark","100"]};
-export const CompBtnHoverBackgroundColor = "color.blue";
 export const ColorBlue = "#3f86f5"; // Color used for logos / highlights
 export const ColorGray = "#bebdbf"; // a midrange gray backgrounds/muted text
 export const ColorLightGray = "#efefef"; // light background color
 export const ColorDark25 = "#707070"; // highlight text
 export const ColorDark50 = "#2a2a2a"; // default text
 export const ColorDark100 = "#232323"; // Element background etc
+export const ComponentsBtnPadding = "1.25rem 5rem";
+export const ComponentsBtnFontSize = "1.07rem";
+export const ComponentsBtnBackgroundColor = {"value":"#232323","comment":"Element background etc","original":{"value":"#232323","comment":"Element background etc"},"name":"ColorDark100","attributes":{"category":"color","type":"dark","item":"100"},"path":["color","dark","100"]};
+export const ComponentsBtnHoverBackgroundColor = "color.blue";
 export const ColorKey = "colors";
 export const ButtonKey = "btns";
 export const HeaderKey = "headers";
