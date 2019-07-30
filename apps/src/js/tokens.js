8c84692f0ed53cf9b01cda33e0a653fccab16b37
shawnsandy <shawnsandy04@gmail.com>
2019-07-30 10:34:56 -0400

    Adds tokens for default/blacks/whites - update mxi base functions

--- apps/src/js/tokens.js
+++ apps/src/js/tokens.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Sun, 28 Jul 2019 12:35:43 GMT
+ * Generated on Mon, 29 Jul 2019 03:26:19 GMT
  */
 
 export const ColorBlue = "#3f86f5"; // Color used for logos / highlights
@@ -9,6 +9,20 @@ export const ColorLightGray = "#efefef"; // light background color
 export const ColorDark25 = "#707070"; // highlight text
 export const ColorDark50 = "#2a2a2a"; // default text
 export const ColorDark100 = "#232323"; // Element background etc
+export const Black25Color = "transparentize(#000000, .75) !important";
+export const Black50Color = "transparentize(#000000, .50) !important";
+export const Black75Color = "transparentize(#000000, .25) !important";
+export const BlackColor = "#000000";
+export const GrayColor = "#000000";
+export const RedColor = "#000000";
+export const BlueColor = "#000000";
+export const OrangeColor = "#000000";
+export const YellowColor = "#000000";
+export const GreenColor = "#000000";
+export const White25Color = "transparentize(#ffffff, .75) !important";
+export const White50Color = "transparentize(#ffffff, .50) !important";
+export const White75Color = "transparentize(#ffffff, .25) !important";
+export const WhiteColor = "#ffffff";
 export const ComponentsBtnPadding = "1.25rem 5rem";
 export const ComponentsBtnFontSize = "1.07rem";
 export const ComponentsBtnBackgroundColor = "#232323";
@@ -19,5 +33,3 @@ export const ComponentsBtnBlueBackgroundColor = "#3f86f5";
 export const ComponentsBtnCursor = "pointer";
 export const ComponentsBtnBorder = "none";
 export const ComponentsBtnBorderRadius = "9999rem";
-export const ColorsBlueColor = "#3f86f5";
-export const ColorsGreenColor = "#3f86f5";
