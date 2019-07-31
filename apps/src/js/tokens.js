529a297b3b7f9645730b7d946e6a17e6ab1a1313
shawnsandy <shawnsandy04@gmail.com>
2019-07-31 10:49:55 -0400

    fixes issues with color-mapping nested maps

--- apps/src/js/tokens.js
+++ apps/src/js/tokens.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 31 Jul 2019 05:32:51 GMT
+ * Generated on Wed, 31 Jul 2019 11:29:40 GMT
  */
 
 export const ColorBlue = "#3f86f5"; // Color used for logos / highlights
@@ -9,14 +9,16 @@ export const ColorLightGray = "#efefef"; // light background color
 export const ColorDark25 = "#707070"; // highlight text
 export const ColorDark50 = "#2a2a2a"; // default text
 export const ColorDark100 = "#232323"; // Element background etc
-export const ColorRedColor = "#d73a49";
-export const ColorOrangeColor = "#f66a0a";
-export const ColorYellowColor = "#ffd33d";
-export const ColorGreenColor = "#28a745";
 export const Black25Color = "transparentize(#000000, .75) !important";
 export const Black50Color = "transparentize(#000000, .50) !important";
 export const Black75Color = "transparentize(#000000, .25) !important";
 export const BlackColor = "#000000";
+export const GrayColor = "#6a737d";
+export const RedColor = "#d73a49";
+export const BlueColor = "#0366d6";
+export const OrangeColor = "#f66a0a";
+export const YellowColor = "#ffd33d";
+export const GreenColor = "#28a745";
 export const White25Color = "transparentize(#ffffff, .75) !important";
 export const White50Color = "transparentize(#ffffff, .50) !important";
 export const White75Color = "transparentize(#ffffff, .25) !important";
