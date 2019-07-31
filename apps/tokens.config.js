3c3f73dbffe50f0dff77264656f4754e628a19ae
shawnsandy <shawnsandy04@gmail.com>
2019-07-31 15:51:33 -0400

    Updates color keys - prefixes tokens with underscore - add color keys based on github pallete - builds tokens

--- apps/tokens.config.js
+++ apps/tokens.config.js
@@ -1,40 +1,40 @@
-const StyleDictionary = require('style-dictionary')
+const StyleDictionary = require("style-dictionary");
 
 const propertiesToCTI = {
-  'width': { category: 'size', type: 'dimension' },
-  'min-width': { category: 'size', type: 'dimension' },
-  'max-width': { category: 'size', type: 'dimension' },
-  'height': { category: 'size', type: 'dimension' },
-  'min-height': { category: 'size', type: 'dimension' },
-  'max-height': { category: 'size', type: 'dimension' },
-  'border-width': { category: 'size', type: 'border', item: 'width' },
-  'border-radius': { category: 'size', type: 'border', item: 'width' },
-  'border-color': { category: 'color', type: 'border' },
-  'background-color': { category: 'color', type: 'background' },
-  'color': { category: 'color', type: 'font' },
-  'text-color': { category: 'color', type: 'font' },
-  'padding': { category: 'size', type: 'padding' },
-  'padding-vertical': { category: 'size', type: 'padding' },
-  'padding-horziontal': { category: 'size', type: 'padding' },
-  'icon': { category: 'content', type: 'icon' },
-  'font-size': { category: 'size', type: 'font' },
-  'line-height': { category: 'size', type: 'line-height' },
-  'size': { category: 'size', type: 'icon' }
-}
+  width: { category: "size", type: "dimension" },
+  "min-width": { category: "size", type: "dimension" },
+  "max-width": { category: "size", type: "dimension" },
+  height: { category: "size", type: "dimension" },
+  "min-height": { category: "size", type: "dimension" },
+  "max-height": { category: "size", type: "dimension" },
+  "border-width": { category: "size", type: "border", item: "width" },
+  "border-radius": { category: "size", type: "border", item: "width" },
+  "border-color": { category: "color", type: "border" },
+  "background-color": { category: "color", type: "background" },
+  color: { category: "color", type: "font" },
+  "text-color": { category: "color", type: "font" },
+  padding: { category: "size", type: "padding" },
+  "padding-vertical": { category: "size", type: "padding" },
+  "padding-horziontal": { category: "size", type: "padding" },
+  icon: { category: "content", type: "icon" },
+  "font-size": { category: "size", type: "font" },
+  "line-height": { category: "size", type: "line-height" },
+  size: { category: "size", type: "icon" }
+};
 
 const CTITransform = {
-  transformer: (prop) => {
+  transformer: prop => {
     // Only do this custom functionality in the 'component' top-level namespace.
-    if (prop.path[0] === 'component') {
+    if (prop.path[0] === "component") {
       // When defining component tokens, the key of the token is the relevant CSS property
       // The key of the token is the last element in the path array
-      return propertiesToCTI[prop.path[prop.path.length - 1]]
+      return propertiesToCTI[prop.path[prop.path.length - 1]];
     } else {
       // Fallback to the original 'attribute/cti' transformer
-      return StyleDictionary.transform['attribute/cti'].transformer(prop)
+      return StyleDictionary.transform["attribute/cti"].transformer(prop);
     }
   }
-}
+};
 
 module.exports = {
   // Rather than calling .registerTransform() we can apply the new transform
@@ -42,52 +42,56 @@ module.exports = {
   // transform name, 'attribute/cti', would work as well.
   transform: {
     // Override the attribute/cti transform
-    'attribute/cti': CTITransform
+    "attribute/cti": CTITransform
   },
-  source: [
-    './src/_data/tokens/**/*.json'
-  ],
+  source: ["./src/_data/tokens/**/*.json"],
   platforms: {
     scss: {
       // We can still use this transformGroup because we are overriding
       // the underlying transform
-      transformGroup: 'scss',
-      buildPath: './src/scss/',
-      prefix: 'sg',
-      files: [{
-        destination: 'tokens.scss',
-        format: 'scss/map-deep'
-      }]
+      transformGroup: "scss",
+      buildPath: "./src/scss/",
+      prefix: "sg",
+      files: [
+        {
+          destination: "_tokens.scss",
+          format: "scss/map-deep"
+        }
+      ]
     },
     flat: {
       // We can still use this transformGroup because we are overriding
       // the underlying transform
-      transformGroup: 'scss',
-      buildPath: './src/scss/',
-      prefix: 'sg',
-      files: [{
-        destination: 'variables.scss',
-        format: 'scss/map-flat'
-      }]
+      transformGroup: "scss",
+      buildPath: "./src/scss/",
+      prefix: "sg",
+      files: [
+        {
+          destination: "_variables.scss",
+          format: "scss/map-flat"
+        }
+      ]
     },
     js: {
       transformGroup: "js",
       buildPath: "./src/js/",
       files: [
         {
-          destination: "tokens.js",
+          destination: "_tokens.js",
           format: "javascript/es6"
         }
       ]
     },
     json: {
-      transformGroup: 'scss',
-      buildPath: './src/json/',
-      prefix: 'sg',
-      files: [{
-        destination: 'tokens.json',
-        format: 'json/nested'
-      }]
+      transformGroup: "scss",
+      buildPath: "./src/json/",
+      prefix: "sg",
+      files: [
+        {
+          destination: "_tokens.json",
+          format: "json/nested"
+        }
+      ]
     }
   }
-}
+};
