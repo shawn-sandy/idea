2bc39d0860e596c403b2cd4804bb6e016ae5d957
shawnsandy <shawnsandy04@gmail.com>
2019-08-05 12:43:32 -0400

    Shortcode--color swatch - reanames ui-tokens to mix-shortcodes

--- packages/ideas/lib/ui-tokens.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  swatch: (color = "#000000", classes = "p-5 rounded shadow-lg") => {
-    return `<div class="${classes}"
-    style="background-color: ${color}"
-  ></div>
-  `;
-  }
-};
