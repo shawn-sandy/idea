e4be2a685dd3dc2b8522321792bbfd0aaef7ba5c
shawnsandy <shawnsandy04@gmail.com>
2019-08-09 20:56:38 -0400

    build output

---
+++ apps/src/js/tokens/tokens.js
@@ -0,0 +1,38 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 31 Jul 2019 19:42:13 GMT
+ */
+
+export const ColorGray = "#6a737d";
+export const ColorRed = "#d73a49";
+export const ColorBlue = "#0366d6";
+export const ColorOrange = "#f66a0a";
+export const ColorYellow = "#ffd33d";
+export const ColorGreen = "#28a745";
+export const ColorDark25 = "#707070"; // highlight text
+export const ColorDark50 = "#2a2a2a"; // default text
+export const ColorDark100 = "#232323"; // Element background etc
+export const Black25Color = "transparentize(#000000, .75) !important";
+export const Black50Color = "transparentize(#000000, .50) !important";
+export const Black75Color = "transparentize(#000000, .25) !important";
+export const BlackColor = "#000000";
+export const GrayColor = "#6a737d";
+export const RedColor = "#d73a49";
+export const BlueColor = "#0366d6";
+export const OrangeColor = "#f66a0a";
+export const YellowColor = "#ffd33d";
+export const GreenColor = "#28a745";
+export const White25Color = "transparentize(#ffffff, .75) !important";
+export const White50Color = "transparentize(#ffffff, .50) !important";
+export const White75Color = "transparentize(#ffffff, .25) !important";
+export const WhiteColor = "#ffffff";
+export const ComponentsBtnPadding = "1.25rem 5rem";
+export const ComponentsBtnFontSize = "1.07rem";
+export const ComponentsBtnBackgroundColor = "#232323";
+export const ComponentsBtnColor = "transparentize(#6a737d, .25)";
+export const ComponentsBtnHoverColor = "darken(transparentize(#6a737d, .25), 10%)";
+export const ComponentsBtnHoverBackgroundColor = "darken(#232323, 10%)";
+export const ComponentsBtnBlueBackgroundColor = "#0366d6";
+export const ComponentsBtnCursor = "pointer";
+export const ComponentsBtnBorder = "none";
+export const ComponentsBtnBorderRadius = "9999rem";
