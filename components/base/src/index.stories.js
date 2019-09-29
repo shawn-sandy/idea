01ef187a0332425aafeedbf08229058dd2fc00ba
shawnsandy <shawnsandy04@gmail.com>
2019-09-29 10:11:48 -0400

    WIP storybook.js integration

---
+++ components/base/src/index.stories.js
@@ -0,0 +1,12 @@
+export default { title: "Button" };
+
+export const simpleButton = () =>
+  `<button style="padding: 10px 30px">😀 Another Button</button>`;
+
+export const withText = () => '<button class="btn">Hello Worlds</button>';
+
+// export const withEmoji = () => {
+//   const button = document.createElement("button");
+//   button.innerText = "😀 😎 👍 💯";
+//   return button;
+// };
