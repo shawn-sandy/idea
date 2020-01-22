b496530a73ef2933f7d98c0a5daf888b26e029bf
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-01-22 06:43:20 -0500

    Refactors directories - add /core - removes components replaced with core/vue-apps -

---
+++ apps/bootstrap/__tests__/bootstrap.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const bootstrap = require('..');
+
+describe('bootstrap', () => {
+    it('needs tests');
+});
---
+++ apps/bootstrap/lib/bootstrap.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = bootstrap;
+
+function bootstrap() {
+    // TODO
+}
